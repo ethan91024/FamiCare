5b45c0529dda140cb25cabeac96007e63cf15edf
ethan <ethan16899@gmail.com>
2022-12-15 01:07:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/seniorprojectapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.seniorprojectapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    TextView hello=findViewById(R.id.homehello);
+                    hello.setText("");
+                    //用switch分析被按下的item
+                    switch(item.getItemId()){
+                        case R.id.navigation_health:
+                            selectedFragment = new HealthFragment();
+                            break;
+                        case R.id.navigation_group:
+                            selectedFragment = new GroupFragment();
+                            break;
+                        case R.id.navigation_mood:
+                            selectedFragment = new MoodFragment();
+                            break;
+                        case R.id.navigation_diary:
+                            selectedFragment = new DiaryFragment();
+                            break;
+                        case R.id.navigation_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
