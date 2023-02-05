97aaa42ddd262cdaed4d6ee6c8caecff00f1da35
ethan <ethan16899@gmail.com>
2023-02-05 18:59:45 +0800

    MainActivity改成kotlin版，跳向下載頁面問題解決，會跳出授權頁面，設定的Health Connect暫時沒用

--- app/src/main/java/com/ethan/FamiCare/MainActivity.kt
+++ app/src/main/java/com/ethan/FamiCare/MainActivity.kt
@@ -1,90 +1,128 @@
-package com.ethan.FamiCare;
+package com.ethan.FamiCare
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.PermissionController
+import androidx.health.connect.client.permission.HealthPermission
+import androidx.health.connect.client.records.*
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.launch
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.health.connect.client.HealthConnectClient;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class MainActivity extends AppCompatActivity {
-    private HealthConnectClient healthConnectClient;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
+class MainActivity : AppCompatActivity() {
+    private val healthConnectClient: HealthConnectClient? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setOnNavigationItemSelectedListener(navListener)
 
         //啟動直接在群組
-        FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction().addToBackStack(null).replace(R.id.fragment_container, new GroupFragment()).commit();
-
-        checkAvailability();
+        val fm = supportFragmentManager
+        fm.beginTransaction().addToBackStack(null).replace(R.id.fragment_container, GroupFragment())
+            .commit()
+        checkAvailability()
     }
 
     //判斷HealthConnect是否安裝了
-    void checkAvailability() {
-        if (HealthConnectClient.isProviderAvailable(MainActivity.this)) {
-            Toast.makeText(MainActivity.this, "Health Connect is installed and supported!", Toast.LENGTH_LONG).show();
+    fun checkAvailability() {
+        if (HealthConnectClient.isProviderAvailable(this@MainActivity)) {
+            Toast.makeText(
+                this@MainActivity,
+                "Health Connect is installed and supported!",
+                Toast.LENGTH_SHORT
+            ).show()
+            checkPermissionsAndRun()
         } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
-            Toast.makeText(MainActivity.this, "Health Connect is not supported!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this@MainActivity, "Health Connect is not supported!", Toast.LENGTH_SHORT)
+                .show()
         } else {
-            Toast.makeText(MainActivity.this, "Health Connect is not installed!", Toast.LENGTH_LONG).show();
-            installHealthConnect();
+            Toast.makeText(this@MainActivity, "Health Connect is not installed!", Toast.LENGTH_SHORT)
+                .show()
+            installHealthConnect()
+            checkPermissionsAndRun()
         }
     }
 
-    //----!----導向下載頁面的method沒反應(只有我)
-    void installHealthConnect() {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        Uri data = Uri.parse("market://details")
-                .buildUpon()
-                .appendQueryParameter("id", "com.google.android.apps.healthdata")
-                .appendQueryParameter("url", "healthconnect://onboarding")
-                .build();
-        intent.setData(data);
-        startActivity(intent);
+    fun installHealthConnect() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_VIEW
+        val data = Uri.parse("market://details")
+            .buildUpon()
+            .appendQueryParameter("id", "com.google.android.apps.healthdata")
+            .appendQueryParameter("url", "healthconnect://onboarding")
+            .build()
+        intent.data = data
+        startActivity(intent)
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
-                    //用switch分析被按下的item
-                    switch (item.getItemId()) {
-                        case R.id.navigation_health:
-                            selectedFragment = new HealthFragment();
-                            break;
-                        case R.id.navigation_group:
-                            selectedFragment = new GroupFragment();
-                            break;
-                        case R.id.navigation_mood:
-                            selectedFragment = new MoodFragment();
-                            break;
-                        case R.id.navigation_diary:
-                            selectedFragment = new DiaryFragment();
-                            break;
-                        case R.id.navigation_settings:
-                            selectedFragment = new SettingsFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
+    fun checkPermissionsAndRun() {
 
-                    return true;
+        val client = HealthConnectClient.getOrCreate(this)
+
+        val permissionsSet = setOf(
+            HealthPermission.getReadPermission(HeartRateRecord::class),
+            HealthPermission.getWritePermission(HeartRateRecord::class),
+            HealthPermission.getReadPermission(HeightRecord::class),
+            HealthPermission.getWritePermission(HeightRecord::class),
+            HealthPermission.getReadPermission(WeightRecord::class),
+            HealthPermission.getWritePermission(WeightRecord::class),
+            HealthPermission.getReadPermission(OxygenSaturationRecord::class),
+            HealthPermission.getWritePermission(OxygenSaturationRecord::class),
+            HealthPermission.getReadPermission(SleepSessionRecord::class),
+            HealthPermission.getWritePermission(SleepSessionRecord::class),
+            HealthPermission.getReadPermission(SleepStageRecord::class),
+            HealthPermission.getWritePermission(SleepStageRecord::class),
+        )
+
+        val requestPermissionActivityContract =
+            PermissionController.createRequestPermissionResultContract()
+
+        val requestPermissions = registerForActivityResult(requestPermissionActivityContract) { granted ->
+            if (granted.containsAll(permissionsSet)) {
+                lifecycleScope.launch {
+                    onPermissionAvailable(client)
                 }
-            };
+            } else {
+                Toast.makeText(
+                    this, "Permissions not granted", Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        lifecycleScope.launch {
+            val granted = client.permissionController
+                .getGrantedPermissions()
+            if (granted.containsAll(permissionsSet)) {
+                onPermissionAvailable(client)
+            } else {
+                requestPermissions.launch(permissionsSet)
+            }
+        }
+    }
+
+    private suspend fun onPermissionAvailable(client: HealthConnectClient) {
+        // todo: read data
+    }
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFragment: Fragment? = null
+        when (item.itemId) {
+            R.id.navigation_health -> selectedFragment = HealthFragment()
+            R.id.navigation_group -> selectedFragment = GroupFragment()
+            R.id.navigation_mood -> selectedFragment = MoodFragment()
+            R.id.navigation_diary -> selectedFragment = DiaryFragment()
+            R.id.navigation_settings -> selectedFragment = SettingsFragment()
+        }
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container,
+            selectedFragment!!
+        ).commit()
+        true
+    }
 }
