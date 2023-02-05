8bc55c157d1bdf9b8ae55a2e8560796b781569aa
ethan <ethan16899@gmail.com>
2023-02-05 18:28:22 +0800

    Health Connect的registerForActivityResult跑不了(On按鈕)

--- app/src/main/java/com/ethan/FamiCare/HealthConnect.kt
+++ app/src/main/java/com/ethan/FamiCare/HealthConnect.kt
@@ -73,7 +73,7 @@ class HealthConnect : AppCompatActivity() {
                 ).show()
             }
         }
-
+//
         // 4
         lifecycleScope.launch {
             val granted = client.permissionController
