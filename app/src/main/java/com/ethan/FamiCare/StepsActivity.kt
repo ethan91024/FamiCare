9c01a850bbb92d22ed4654c92ac4d36fd99f36ea
Alvin9840 <alvin9840@gmail.com>
2023-02-12 17:25:53 +0800

    日記版面調整，讀到步數了

--- app/src/main/java/com/ethan/FamiCare/StepsActivity.kt
+++ app/src/main/java/com/ethan/FamiCare/StepsActivity.kt
@@ -6,8 +6,12 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.request.ReadRecordsRequest
+import androidx.health.connect.client.time.TimeRangeFilter
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
+import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.temporal.ChronoUnit
@@ -22,7 +26,7 @@ class StepsActivity : AppCompatActivity() {
             lifecycleScope.launch {
                 val fragment: Fragment
                 fragment = HealthFragment()
-                val steps = fragment.getStepCount(
+                val steps = getStepCount(
                     client,
                     LocalDateTime.now().truncatedTo(ChronoUnit.DAYS)
                         .toInstant(ZoneOffset.ofHours(8)),
@@ -31,15 +35,29 @@ class StepsActivity : AppCompatActivity() {
                 val first: TextView
                 first = findViewById(R.id.today)
                 first.text = steps[0].count.toString()
-                val yesterday: TextView
-                yesterday = findViewById(R.id.yesterday)
-                yesterday.text = steps[1].count.toString()
-                val twodaysago: TextView
-                twodaysago = findViewById(R.id.twodaysago)
-                twodaysago.text = steps[2].count.toString()
+//                val yesterday: TextView
+//                yesterday = findViewById(R.id.yesterday)
+//                yesterday.text = steps[1].count.toString()
+//                val twodaysago: TextView
+//                twodaysago = findViewById(R.id.twodaysago)
+//                twodaysago.text = steps[2].count.toString()
             }
         }
     }
 
+    suspend fun getStepCount(
+        client: HealthConnectClient,
+        start: Instant,
+        end: Instant
+    ): List<StepsRecord> {
+
+        val request = client.readRecords(
+            ReadRecordsRequest(
+                StepsRecord::class,
+                timeRangeFilter = TimeRangeFilter.between(start, end)
+            )
+        )
+        return request.records
+    }
 
 }
