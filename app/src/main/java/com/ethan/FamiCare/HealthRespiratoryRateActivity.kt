48880210ac9a1b99110daf80d32c4d85653fcb73
alvin <alvin9840@gmail.com>
2023-02-13 23:44:06 +0800

    健康版面弄一弄

---
+++ app/src/main/java/com/ethan/FamiCare/HealthRespiratoryRateActivity.kt
@@ -0,0 +1,11 @@
+package com.ethan.FamiCare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HealthRespiratoryRateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_health_respiratory_rate)
+    }
+}
