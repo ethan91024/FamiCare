cd3e9aff500c19447188f539ea5f468f6db0fb71
alvin <alvin9840@gmail.com>
2023-02-13 14:37:22 +0800

    健康每個按鈕都連好

---
+++ app/src/main/java/com/ethan/FamiCare/HealthRespiratoryRateActivity.java
@@ -0,0 +1,14 @@
+package com.ethan.FamiCare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HealthRespiratoryRateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_respiratory_rate);
+    }
+}
