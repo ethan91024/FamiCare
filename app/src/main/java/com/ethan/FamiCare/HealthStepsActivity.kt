747ebe8822e06932e89173f60e700ca4f06ab85c
ethan <ethan16899@gmail.com>
2023-02-14 16:08:53 +0800

    步數的平均搞出來了，但method因為資料型態的關係不能回傳數字，所以我先把更改TextView放在method裡，之後要從別的地方呼叫資料要再回來改

--- app/src/main/java/com/ethan/FamiCare/HealthStepsActivity.kt
+++ app/src/main/java/com/ethan/FamiCare/HealthStepsActivity.kt
@@ -7,15 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.health.connect.client.HealthConnectClient
 import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.request.AggregateGroupByPeriodRequest
+import androidx.health.connect.client.request.AggregateRequest
 import androidx.health.connect.client.request.ReadRecordsRequest
 import androidx.health.connect.client.time.TimeRangeFilter
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZoneOffset
+import java.time.*
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
@@ -31,9 +30,8 @@ class HealthStepsActivity : AppCompatActivity() {
         lifecycleScope.launch {
             val steps = getStepCount(
                 client,
-                LocalDateTime.now().minusDays(3).truncatedTo(ChronoUnit.DAYS)
-                    .toInstant(ZoneOffset.ofHours(0)),
-                LocalDateTime.now().toInstant(ZoneOffset.ofHours(0))
+                LocalDateTime.now().minusDays(3),
+                LocalDateTime.now()
             )
             val first: TextView
             first = findViewById(R.id.today)
@@ -44,16 +42,24 @@ class HealthStepsActivity : AppCompatActivity() {
             val twodaysago: TextView
             twodaysago = findViewById(R.id.twodaysago)
             twodaysago.text = myDateTimeFormatter.format(Instant.parse(steps[steps.size-3].endTime.toString()))+"  "+steps[steps.size-3].count.toString()+"步"
+
+            aggregateStepsIntoWeeks(
+                client,
+                LocalDateTime.now().minusDays(7),
+                LocalDateTime.now())
+
+            aggregateStepsIntoMonths(
+                client,
+                LocalDateTime.now().minusDays(30),
+                LocalDateTime.now())
         }
 
         this.findViewById<Button>(R.id.update).setOnClickListener {
             lifecycleScope.launch {
                 val steps = getStepCount(
                     client,
-                    LocalDateTime.now().minusDays(3).truncatedTo(ChronoUnit.DAYS)
-                        .toInstant(ZoneOffset.ofHours(0)),
-                    LocalDateTime.now().toInstant(ZoneOffset.ofHours(0))
-                )
+                    LocalDateTime.now().minusDays(3),
+                    LocalDateTime.now())
                 val first: TextView
                 first = findViewById(R.id.today)
                 first.text = myDateTimeFormatter.format(Instant.parse(steps[steps.size-1].endTime.toString()))+"  "+steps[steps.size-1].count.toString()+"步"
@@ -63,14 +69,24 @@ class HealthStepsActivity : AppCompatActivity() {
                 val twodaysago: TextView
                 twodaysago = findViewById(R.id.twodaysago)
                 twodaysago.text = myDateTimeFormatter.format(Instant.parse(steps[steps.size-3].endTime.toString()))+"  "+steps[steps.size-3].count.toString()+"步"
+
+                aggregateStepsIntoWeeks(
+                    client,
+                    LocalDateTime.now().minusDays(7),
+                    LocalDateTime.now())
+
+                aggregateStepsIntoMonths(
+                    client,
+                    LocalDateTime.now().minusDays(30),
+                    LocalDateTime.now())
             }
         }
     }
 
     suspend fun getStepCount(
         client: HealthConnectClient,
-        start: Instant,
-        end: Instant
+        start: LocalDateTime,
+        end: LocalDateTime
     ): List<StepsRecord> {
 
         val request = client.readRecords(
@@ -80,6 +96,52 @@ class HealthStepsActivity : AppCompatActivity() {
             )
         )
         return request.records
+    }
+
+    suspend fun aggregateStepsIntoWeeks(
+        healthConnectClient: HealthConnectClient,
+        startTime: LocalDateTime,
+        endTime: LocalDateTime
+    ){
+        val response =
+            healthConnectClient.aggregateGroupByPeriod(
+                AggregateGroupByPeriodRequest(
+                    metrics = setOf(StepsRecord.COUNT_TOTAL),
+                    timeRangeFilter = TimeRangeFilter.between(startTime, endTime),
+                    timeRangeSlicer = Period.ofWeeks(1)
+                )
+            )
+
+        for (weeklyResult in response) {
+            val totalSteps = weeklyResult.result[StepsRecord.COUNT_TOTAL]
+            val week: TextView = findViewById(R.id.weekAvg)
+            if (totalSteps != null) {
+                week.text = totalSteps.div(7).toString()+"步"
+            }
+        }
+    }
+
+    suspend fun aggregateStepsIntoMonths(
+        healthConnectClient: HealthConnectClient,
+        startTime: LocalDateTime,
+        endTime: LocalDateTime
+    ) {
+        val response =
+            healthConnectClient.aggregateGroupByPeriod(
+                AggregateGroupByPeriodRequest(
+                    metrics = setOf(StepsRecord.COUNT_TOTAL),
+                    timeRangeFilter = TimeRangeFilter.between(startTime, endTime),
+                    timeRangeSlicer = Period.ofMonths(1)
+                )
+            )
+
+        for (monthlyResult in response) {
+            val totalSteps = monthlyResult.result[StepsRecord.COUNT_TOTAL]
+            val month: TextView = findViewById(R.id.monthAvg)
+            if (totalSteps != null) {
+                month.text = totalSteps.div(30).toString()+"步"
+            }
+        }
     }
 
 }
