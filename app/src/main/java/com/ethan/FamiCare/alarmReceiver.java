4ecbb905dc786e6b175ff8f3a04717915d731586
koonai1210 <409630703@gms.tku.edu.tw>
2023-02-16 16:47:40 +0800

    通知問題

---
+++ app/src/main/java/com/ethan/FamiCare/alarmReceiver.java
@@ -0,0 +1,50 @@
+package com.ethan.FamiCare;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class alarmReceiver extends BroadcastReceiver {
+
+    //private static final String Channel_ID="cal_channel";
+    private  int notificationId=1;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Get id from intent
+
+        String event=intent.getStringExtra("event");
+
+
+        //Call GroupCalendar when notification is tapped
+        Intent main=new Intent(context,GroupCalendar.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent contextTntent=PendingIntent.getActivity(context,0,main,0);
+
+        //NotificationManager
+        //NotificationManager notificationManager=(NotificationManager) context.getSystemService((Context.NOTIFICATION_SERVICE));
+
+        //準備通知
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(context,"calandroid")
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("行程")
+                .setContentText(event)
+                .setContentIntent(contextTntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(context);
+
+        //Notify
+        notificationManagerCompat.notify(notificationId,builder.build());
+
+    }
+}
