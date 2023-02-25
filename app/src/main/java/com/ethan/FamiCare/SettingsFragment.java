739de8f221260685ddbdeebf873305eedb660a1a
andy901120 <yies.andy333@gmail.com>
2023-02-25 22:04:25 +0800

    別人的訊息顏色會不同，google登入還不能用

--- app/src/main/java/com/ethan/FamiCare/SettingsFragment.java
+++ app/src/main/java/com/ethan/FamiCare/SettingsFragment.java
@@ -6,17 +6,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingsFragment extends Fragment {
 
@@ -29,6 +37,9 @@ public class SettingsFragment extends Fragment {
     TextInputLayout email, password;
     Button login, signup;
     FirebaseAuth auth;
+    ImageView google_img;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -43,14 +54,20 @@ public class SettingsFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+
+    public void onCreate(LayoutInflater inflater, ViewGroup container,
+                         Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
+
+
 
 
     @Override
@@ -66,6 +83,24 @@ public class SettingsFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        google_img=view.findViewById(R.id.google);
+
+        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN )
+                .requestEmail()
+                .build();
+
+        gsc= GoogleSignIn.getClient(SettingsFragment.this.getContext(),gso);
+
+        google_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                google_singin();
+            }
+        });
+
+
+
 //1
         Intent intent = new Intent();
         email = view.findViewById(R.id.email);
@@ -85,7 +120,7 @@ public class SettingsFragment extends Fragment {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(getActivity(), "Login successd!", Toast.LENGTH_SHORT).show();
-                            fm.beginTransaction().addToBackStack(null).replace(R.id.setting, new GroupChatroom()).commit();
+                            fm.beginTransaction().setReorderingAllowed(true).addToBackStack(null).replace(R.id.setting, new AfterLogin()).commit();
 
                         } else {
                             Toast.makeText(getActivity(), "Login Failed!", Toast.LENGTH_SHORT).show();
@@ -115,14 +150,40 @@ public class SettingsFragment extends Fragment {
         });
         return view;
     }
+    private void google_singin() {
+
+        Intent intent=gsc.getSignInIntent();
+        startActivityForResult(intent,100);
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        if(requestCode==100){
+            Task<GoogleSignInAccount>task=GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                fm.beginTransaction().setReorderingAllowed(true).addToBackStack(null).replace(R.id.setting, new AfterLogin()).commit();
+
+            } catch (ApiException e) {
+                Toast.makeText(SettingsFragment.this.getContext(), "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 /*
     @Override
     public void onStart() {
         FragmentManager fm = getActivity().getSupportFragmentManager();
         super.onStart();
-        if(auth!=null ){
-            fm.beginTransaction().replace(R.id.setting,new GroupChatroom()).commit();
+        FirebaseUser user=auth.getCurrentUser();
+        if(user!=null ){
+            fm.beginTransaction().setReorderingAllowed(true).replace(R.id.setting,new AfterLogin()).commit();
         }
     }
+
  */
+
 }
+
