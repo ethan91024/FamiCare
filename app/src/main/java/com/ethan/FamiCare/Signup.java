862cfc026f7d7b870743c75b598833bd2a05b526
andy901120 <yies.andy333@gmail.com>
2023-03-28 21:53:34 +0800

    好友名字有顯示了

--- app/src/main/java/com/ethan/FamiCare/Signup.java
+++ app/src/main/java/com/ethan/FamiCare/Signup.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ethan.FamiCare.Firebasecords.Users;
+import com.ethan.FamiCare.databinding.ActivitySignupBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
@@ -21,6 +22,7 @@ public class Signup extends AppCompatActivity {
     public Signup() {
         // Required empty public constructor
     }
+    ActivitySignupBinding binding;
     TextInputLayout email, password,username;
     Button cancel, signup;
     FirebaseAuth auth;
@@ -30,12 +32,17 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_signup2);
+        setContentView(R.layout.activity_signup);
+
+
+        username=findViewById(R.id.username);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         cancel= findViewById(R.id.cancel);
         signup = findViewById(R.id.signupb);
+
         auth = FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,22 +59,27 @@ public class Signup extends AppCompatActivity {
                 String u = username.getEditText().getText().toString();
                 String e = email.getEditText().getText().toString();
                 String p = password.getEditText().getText().toString();
-                auth.createUserWithEmailAndPassword(e, p).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Users user=new Users(u,e,p);
-                            String id=task.getResult().getUser().getUid();
-                            database.getInstance().getReference().child("Users").push().child(id).setValue(user);
-                            Toast.makeText(Signup.this, "Account Created Successful", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(Signup.this, GroupFragment.class);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(Signup.this, "Failed!" + task.getException(), Toast.LENGTH_LONG).show();
+                if(!u.isEmpty()&&!e.isEmpty()&&!p.isEmpty()) {
+                    auth.createUserWithEmailAndPassword(e,p).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Users user = new Users(u, e, p);
+                                String id = task.getResult().getUser().getUid();
+                                database.getReference().child("Users").child(id).setValue(user);
+                                Toast.makeText(Signup.this, "Account Created Successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Signup.this, GroupFragment.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(Signup.this, "Sing up Failed!" + task.getException(), Toast.LENGTH_LONG).show();
 
+                            }
                         }
-                    }
-                });
+                    });
+                }else{
+                    Toast.makeText(Signup.this, "Failed!" , Toast.LENGTH_LONG).show();
+
+                }
             }
         });
     }
