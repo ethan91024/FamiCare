1141adfbb774e278aadac66137d86dfe088a73ba
Alvin9840 <alvin9840@gmail.com>
2023-07-11 18:03:05 +0800

    幫潘弄照片的，但還不能把照片貼到聊天室

--- app/src/main/java/com/ethan/FamiCare/Group/GroupChatroom.java
+++ app/src/main/java/com/ethan/FamiCare/Group/GroupChatroom.java
@@ -2,10 +2,15 @@
 package com.ethan.FamiCare.Group;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,28 +22,38 @@ import com.ethan.FamiCare.Firebasecords.MessageModel;
 import com.ethan.FamiCare.MainActivity;
 import com.ethan.FamiCare.R;
 import com.ethan.FamiCare.databinding.FragmentGroupChatroomBinding;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class GroupChatroom extends AppCompatActivity {
     ChatAdapter adapter;
     RecyclerView recyclerView;//1
-    final ArrayList<MessageModel> list =new ArrayList<>();
+    final ArrayList<MessageModel> list = new ArrayList<>();
     FragmentGroupChatroomBinding binding;
     FirebaseAuth auth;
     FirebaseDatabase database;
-    ImageView photo,camera;
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+    ImageView photo, camera;
+    private static final int TAKE_PHOTO_REQUEST = 0;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Uri selectedImageUri;
     RelativeLayout containerLayout;
 
 
@@ -47,14 +62,15 @@ public class GroupChatroom extends AppCompatActivity {
     }
 
 
-
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = FragmentGroupChatroomBinding.inflate(getLayoutInflater());
         database = FirebaseDatabase.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Grouplist");
+        storageReference = FirebaseStorage.getInstance().getReference("Grouplist");
         auth = FirebaseAuth.getInstance();
+
         setContentView(binding.getRoot());
         final String senderId = auth.getUid();
         String recieveId = getIntent().getStringExtra("userId");
@@ -62,8 +78,8 @@ public class GroupChatroom extends AppCompatActivity {
         String profilePic = getIntent().getStringExtra("profilePic");
 
         containerLayout = findViewById(R.id.groupchatroom);
-        camera=findViewById(R.id.camera);
-        photo=findViewById(R.id.photo);
+        camera = findViewById(R.id.camera);
+        photo = findViewById(R.id.photo);
 
         binding.username.setText(userName);
         Picasso.get().load(profilePic).placeholder(R.drawable.avatar_b).into(binding.profileImage);
@@ -78,19 +94,19 @@ public class GroupChatroom extends AppCompatActivity {
         final ArrayList<MessageModel> messageModels = new ArrayList<>();
         final ChatAdapter chatAdapter = new ChatAdapter(messageModels, this, recieveId);
         binding.recyclerview.setAdapter(chatAdapter);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setStackFromEnd(true);
         binding.recyclerview.setLayoutManager(layoutManager);
 
-        final String senderRoom=senderId+recieveId;
-        final String receiverRoom=recieveId+senderId;
+        final String senderRoom = senderId + recieveId;
+        final String receiverRoom = recieveId + senderId;
 
         database.getReference().child("chats").child(senderRoom).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 messageModels.clear();
-                for (DataSnapshot snapshot1:snapshot.getChildren()){
-                    MessageModel model=snapshot1.getValue(MessageModel.class);
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    MessageModel model = snapshot1.getValue(MessageModel.class);
                     model.setMessageId(snapshot1.getKey());
                     messageModels.add(model);
                 }
@@ -105,8 +121,8 @@ public class GroupChatroom extends AppCompatActivity {
         binding.fabSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String message=binding.message.getText().toString();
-                final MessageModel model=new MessageModel(senderId,message);
+                String message = binding.message.getText().toString();
+                final MessageModel model = new MessageModel(senderId, message);
                 model.setDatetime(new Date().getTime());
                 binding.message.setText("");
 
@@ -121,6 +137,88 @@ public class GroupChatroom extends AppCompatActivity {
                         });
                     }
                 });
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, TAKE_PHOTO_REQUEST);
+            }
+        });
+
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
+    }
+
+    // 跳转到相机或是相册
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            selectedImageUri = data.getData();
+            savePhoto(selectedImageUri);
+
+        } else if (requestCode == TAKE_PHOTO_REQUEST && resultCode == RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap photoBitmap = (Bitmap) extras.get("data");
+                selectedImageUri = savePhotoToFile(photoBitmap);
+                savePhoto(selectedImageUri);
+            }
+        }
+    }
+
+    //把照片暫存在本地端，為了轉成Uri
+    private Uri savePhotoToFile(Bitmap bitmap) {
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File photoFile = null;
+        try {
+            photoFile = File.createTempFile(
+                    "temp_photo",
+                    ".jpg",
+                    storageDir
+            );
+            FileOutputStream outputStream = new FileOutputStream(photoFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Uri.fromFile(photoFile);
+    }
+
+    // 照片存到資料庫
+    public void savePhoto(Uri uri) {
+        StorageReference storageRef = storageReference.child(System.currentTimeMillis() + ".jpg");
+
+        UploadTask uploadTask = storageRef.putFile(uri);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // 照片上传成功
+                // 获取照片的下载URL
+                storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        String GroupPhoto = uri.toString();
+//                        database.getReference().child("Grouplist").child(uid).setValue(GroupPhoto);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(GroupChatroom.this, "群組頭像上傳失敗!", Toast.LENGTH_SHORT).show();
             }
         });
     }
