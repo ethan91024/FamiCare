ce3fdfe659cb2ed350c4c6afb8af7d2de1ea6d88
Alvin9840 <alvin9840@gmail.com>
2023-07-11 17:33:26 +0800

    AddNewGroup的頭像可以上傳，位置給潘修

--- app/src/main/java/com/ethan/FamiCare/Group/AddNewGroup.java
+++ app/src/main/java/com/ethan/FamiCare/Group/AddNewGroup.java
@@ -1,21 +1,32 @@
 package com.ethan.FamiCare.Group;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.ethan.FamiCare.Firebasecords.FriendModel;
 import com.ethan.FamiCare.MainActivity;
 import com.ethan.FamiCare.R;
 import com.ethan.FamiCare.databinding.ActivityAddNewGroupBinding;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -23,43 +34,55 @@ public class AddNewGroup extends AppCompatActivity {
 
     FirebaseAuth auth;
     FirebaseDatabase database;
-    Button add,cancel;
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+    Button add, cancel;
     TextView edittext;
     CircleImageView editprofileimage;
     ActivityAddNewGroupBinding binding;
     String uid;//使用者的uid
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_group);
-        binding= ActivityAddNewGroupBinding.inflate(getLayoutInflater());
+        binding = ActivityAddNewGroupBinding.inflate(getLayoutInflater());
 
-        add=findViewById(R.id.add);
-        cancel=findViewById(R.id.cancel);
-        edittext=findViewById(R.id.groupname);
+        add = findViewById(R.id.add);
+        cancel = findViewById(R.id.cancel);
+        edittext = findViewById(R.id.groupname);
         editprofileimage = findViewById(R.id.profile_image);
 
-        auth=FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
-        uid=auth.getCurrentUser().getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Grouplist");
+        storageReference = FirebaseStorage.getInstance().getReference("Grouplist");
+        uid = auth.getCurrentUser().getUid();
+
+        editprofileimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callAlertDialog();
+            }
+        });
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (edittext.getText().toString().isEmpty()){
-                    Toast.makeText(AddNewGroup.this, "請輸入群組名稱 " , Toast.LENGTH_SHORT).show();
-                }else {
-                    String groupname=edittext.getText().toString();
-                    FriendModel friend = new FriendModel( groupname);
+                if (edittext.getText().toString().isEmpty()) {
+                    Toast.makeText(AddNewGroup.this, "請輸入群組名稱 ", Toast.LENGTH_SHORT).show();
+                } else {
+                    String groupname = edittext.getText().toString();
+                    FriendModel friend = new FriendModel(groupname);
                     database.getReference().child("Grouplist").child(uid).push().setValue(friend)
                             .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Toast.makeText(AddNewGroup.this, "群組創建成功", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(AddNewGroup.this, MainActivity.class);
-                            startActivity(intent);
-                        }
-                    });
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(AddNewGroup.this, "群組創建成功", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(AddNewGroup.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+                            });
                 }
             }
         });
@@ -68,6 +91,87 @@ public class AddNewGroup extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(AddNewGroup.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+    // 照片相關
+    Bitmap bitmap;
+    private static final int TAKE_PHOTO_REQUEST = 0;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Uri selectedImageUri;
+
+    // 上傳照片到群組頭像或離開
+    private void callAlertDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("設定群組頭像")
+                .setMessage("拍張照或選擇照片?")
+                // 拍照
+                .setPositiveButton("現在拍！", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent, TAKE_PHOTO_REQUEST);
+                    }
+                })
+                // 选择照片
+                .setNegativeButton("選擇照片", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+                    }
+                })
+                // 直接跳回DiaryFragment
+                .setNeutralButton("不用", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        onBackPressed();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    // 跳转到相机或是相册
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            selectedImageUri = data.getData();
+            savePhoto(selectedImageUri);
+
+        } else if (requestCode == TAKE_PHOTO_REQUEST && resultCode == RESULT_OK && data != null) {
+            selectedImageUri = data.getData();
+            savePhoto(selectedImageUri);
+        }
+    }
+
+    // 照片存到資料庫
+    public void savePhoto(Uri uri) {
+        StorageReference storageRef = storageReference.child(System.currentTimeMillis() + ".jpg");
+
+        UploadTask uploadTask = storageRef.putFile(uri);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // 照片上传成功
+                // 获取照片的下载URL
+                storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        String GroupPhoto = uri.toString();
+                        database.getReference().child("Grouplist").child(uid).setValue(GroupPhoto);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AddNewGroup.this, "群組頭像上傳失敗!", Toast.LENGTH_SHORT).show();
             }
         });
     }
